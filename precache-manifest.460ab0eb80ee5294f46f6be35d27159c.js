552028215dcce8907f0a67c633615b2c2a7ddda0
unknown <mohamed.ahmed8412000@gmail.com>
2025-03-10 06:27:39 +0300

    Updates

---
+++ precache-manifest.460ab0eb80ee5294f46f6be35d27159c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c68c897a819a5e762d7806d95f92c47",
+    "url": "/aitco/index.html"
+  },
+  {
+    "revision": "13307af889f911e6bbf2",
+    "url": "/aitco/static/css/2.43e3cdd6.chunk.css"
+  },
+  {
+    "revision": "d66a01542c5c7cb37d8c",
+    "url": "/aitco/static/css/main.9a809218.chunk.css"
+  },
+  {
+    "revision": "13307af889f911e6bbf2",
+    "url": "/aitco/static/js/2.8d3e6d51.chunk.js"
+  },
+  {
+    "revision": "f056774035ce39e6238293d1e83850f9",
+    "url": "/aitco/static/js/2.8d3e6d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66a01542c5c7cb37d8c",
+    "url": "/aitco/static/js/main.7786e0fd.chunk.js"
+  },
+  {
+    "revision": "ae9010809a155c41525c",
+    "url": "/aitco/static/js/runtime-main.48bda9c4.js"
+  },
+  {
+    "revision": "11a8c741b14685ea49efcbe8d1eea176",
+    "url": "/aitco/static/media/ABADIALJOHERProject.11a8c741.png"
+  },
+  {
+    "revision": "493ca2e28168b3fe1fc17d0c9cee3a98",
+    "url": "/aitco/static/media/CityWalkProject.493ca2e2.png"
+  },
+  {
+    "revision": "bf202d75b3bfecbcbb3d12da1d448210",
+    "url": "/aitco/static/media/DiscoveryBLVDProject.bf202d75.png"
+  },
+  {
+    "revision": "834d028ce0d8e700fe0b078ea0e9e2a9",
+    "url": "/aitco/static/media/EquestrianClubProject.834d028c.png"
+  },
+  {
+    "revision": "b8ae5b7169d05e8fafa37ce6e1e133bb",
+    "url": "/aitco/static/media/FALCONGATEProject.b8ae5b71.png"
+  },
+  {
+    "revision": "d34e3eeb47cb5b02ac694de696ad00c5",
+    "url": "/aitco/static/media/MountainBLVDProject.d34e3eeb.png"
+  },
+  {
+    "revision": "615cac161807e693c7ddbe45b25ed883",
+    "url": "/aitco/static/media/NinjaWarriorBLVDProject.615cac16.png"
+  },
+  {
+    "revision": "e1ee4435030069ae5ce87af78447ad07",
+    "url": "/aitco/static/media/OURGOALS.e1ee4435.png"
+  },
+  {
+    "revision": "af3d671f5eaa89a448da50f0ce5745f6",
+    "url": "/aitco/static/media/QAULITYANDSAFTY.af3d671f.png"
+  },
+  {
+    "revision": "c53cc851e7f10595217e928c63807ff9",
+    "url": "/aitco/static/media/SUPERDOMProject.c53cc851.png"
+  },
+  {
+    "revision": "994d1ce5736fb63aeb6088bc88229928",
+    "url": "/aitco/static/media/THEKINGDOMARENAProject.994d1ce5.png"
+  },
+  {
+    "revision": "5ca4c97197c82c8c196d0a43372341cf",
+    "url": "/aitco/static/media/TheVenueProject.5ca4c971.png"
+  },
+  {
+    "revision": "cad5a1bdff325a35c452c903ad5bddfa",
+    "url": "/aitco/static/media/avatar.cad5a1bd.webp"
+  },
+  {
+    "revision": "cad5a1bdff325a35c452c903ad5bddfa",
+    "url": "/aitco/static/media/chan-dung-edit.cad5a1bd.png"
+  },
+  {
+    "revision": "3e73d16f064d925f1076e94ee23930ee",
+    "url": "/aitco/static/media/engsalah1.3e73d16f.png"
+  },
+  {
+    "revision": "b5e8ee0f0a85e0c9403b7fbc61220bb3",
+    "url": "/aitco/static/media/engsalah2.b5e8ee0f.png"
+  },
+  {
+    "revision": "cad5a1bdff325a35c452c903ad5bddfa",
+    "url": "/aitco/static/media/full-nguoi-edit.cad5a1bd.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/aitco/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/aitco/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/aitco/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/aitco/static/media/slick.f97e3bbf.svg"
+  }
+]);
